511ce077809ec04e826c70ef09da137c2610edab
vladt <vladt@sonatype.com>
2011-01-20 14:24:41 -0500

    Log a warning when marker related osgi services are not available

--- org.eclipse.m2e.core/src/org/eclipse/m2e/core/internal/project/MarkerUtils.java
+++ org.eclipse.m2e.core/src/org/eclipse/m2e/core/internal/project/MarkerUtils.java
@@ -12,6 +12,8 @@ package org.eclipse.m2e.core.internal.project;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
@@ -29,15 +31,13 @@ import org.eclipse.m2e.core.project.IMavenMarkerManager;
  * @author mkleint
  */
 public class MarkerUtils {
+  private static Logger log = LoggerFactory.getLogger(MarkerUtils.class);
+
   public static void decorateMarker(IMarker marker) {
     BundleContext context = MavenPlugin.getDefault().getBundleContext();
-    if (context == null) {
-      //eg. in tests
-      return;
-    }
     ServiceReference ref = context.getServiceReference(IMarkerLocationService.class.getName());
     if (ref == null) {
-      //eg. in tests
+      log.warn("Could not find OSGI service for " + IMarkerLocationService.class.getName());
       return;
     }
     IMarkerLocationService service = (IMarkerLocationService)context.getService(ref);
@@ -53,13 +53,9 @@ public class MarkerUtils {
   public static void addEditorHintMarkers(IMavenMarkerManager markerManager, IFile pom, MavenProject mavenProject,
       String type) {
     BundleContext context = MavenPlugin.getDefault().getBundleContext();
-    if (context == null) {
-      //eg. in tests
-      return;
-    }
     ServiceReference ref = context.getServiceReference(IEditorMarkerService.class.getName());
     if (ref == null) {
-      //eg. in tests
+      log.warn("Could not find OSGI service for " + IEditorMarkerService.class.getName());
       return;
     }
     IEditorMarkerService service = (IEditorMarkerService)context.getService(ref);
