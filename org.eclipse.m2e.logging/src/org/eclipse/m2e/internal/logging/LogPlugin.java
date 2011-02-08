97246aa46e8a4b1d8efb8d7583b133dc0166a36a
vladt <vladt@sonatype.com>
2011-02-08 12:01:30 -0500

    Fixed formatting

--- org.eclipse.m2e.logging/src/org/eclipse/m2e/internal/logging/LogPlugin.java
+++ org.eclipse.m2e.logging/src/org/eclipse/m2e/internal/logging/LogPlugin.java
@@ -8,6 +8,7 @@
  * Contributors:
  *      Sonatype, Inc. - initial API and implementation
  *******************************************************************************/
+
 package org.eclipse.m2e.internal.logging;
 
 import java.io.File;
@@ -15,9 +16,6 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.net.URL;
 
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Plugin;
-import org.eclipse.core.runtime.Status;
 import org.osgi.framework.BundleContext;
 import org.slf4j.LoggerFactory;
 
@@ -27,70 +25,74 @@ import ch.qos.logback.classic.util.ContextInitializer;
 import ch.qos.logback.core.joran.spi.JoranException;
 import ch.qos.logback.core.util.StatusPrinter;
 
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Plugin;
+import org.eclipse.core.runtime.Status;
+
+
 public class LogPlugin extends Plugin {
 
-	private static final String ID = "org.eclipse.m2e.logging";
+  private static final String ID = "org.eclipse.m2e.logging";
 
-	@Override
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
+  @Override
+  public void start(BundleContext context) throws Exception {
+    super.start(context);
 
-		configureLogger(context);
-	}
+    configureLogger(context);
+  }
 
-	private void configureLogger(BundleContext context) {
-		if (System.getProperty(ContextInitializer.CONFIG_FILE_PROPERTY) != null) {
-			return;
-		}
+  private void configureLogger(BundleContext context) {
+    if(System.getProperty(ContextInitializer.CONFIG_FILE_PROPERTY) != null) {
+      return;
+    }
 
-		File stateDir = getStateLocation().toFile();
+    File stateDir = getStateLocation().toFile();
 
-		File configFile = new File(stateDir, "logback." + context.getBundle().getVersion().toString() +  ".xml");
+    File configFile = new File(stateDir, "logback." + context.getBundle().getVersion().toString() + ".xml");
 
-		if (!configFile.isFile()) {
-			//Copy the config file
-			try {
-				InputStream is = context.getBundle().getEntry("defaultLogbackConfiguration/logback.xml").openStream();
-				try {
-					configFile.getParentFile().mkdirs();
-					FileOutputStream fos = new FileOutputStream(configFile);
-					try {
-						for (byte[] buffer = new byte[1024 * 4];;) {
-							int n = is.read(buffer);
-							if (n < 0) {
-								break;
-							}
-							fos.write(buffer, 0, n);
-						}
-					} finally {
-						fos.close();
-					}
-				} finally {
-					is.close();
-				}
-				
-				loadConfiguration(configFile.toURL());
-			} catch (Exception e) {
-				getLog().log( new Status(IStatus.WARNING, ID, "Exception while setting up logging.", e));
-				return;
-			}
-		}
-	}
+    if(!configFile.isFile()) {
+      //Copy the config file
+      try {
+        InputStream is = context.getBundle().getEntry("defaultLogbackConfiguration/logback.xml").openStream();
+        try {
+          configFile.getParentFile().mkdirs();
+          FileOutputStream fos = new FileOutputStream(configFile);
+          try {
+            for(byte[] buffer = new byte[1024 * 4];;) {
+              int n = is.read(buffer);
+              if(n < 0) {
+                break;
+              }
+              fos.write(buffer, 0, n);
+            }
+          } finally {
+            fos.close();
+          }
+        } finally {
+          is.close();
+        }
 
-	private void loadConfiguration(URL configFile) throws JoranException {
-		LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
-		lc.reset();
+        loadConfiguration(configFile.toURL());
+      } catch(Exception e) {
+        getLog().log(new Status(IStatus.WARNING, ID, "Exception while setting up logging.", e));
+        return;
+      }
+    }
+  }
 
-		JoranConfigurator configurator = new JoranConfigurator();
-		configurator.setContext(lc);
-		configurator.doConfigure(configFile);
+  private void loadConfiguration(URL configFile) throws JoranException {
+    LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+    lc.reset();
 
-		StatusPrinter.printInCaseOfErrorsOrWarnings(lc);
-	}
+    JoranConfigurator configurator = new JoranConfigurator();
+    configurator.setContext(lc);
+    configurator.doConfigure(configFile);
 
-	@Override
-	public void stop(BundleContext context) throws Exception {
-		super.stop(context);
-	}
+    StatusPrinter.printInCaseOfErrorsOrWarnings(lc);
+  }
 
+  @Override
+  public void stop(BundleContext context) throws Exception {
+    super.stop(context);
+  }
 }
--- org.eclipse.m2e.slf4j.bridge/src/org/eclipse/m2e/internal/slf4j/bridge/LogListener.java
+++ org.eclipse.m2e.slf4j.bridge/src/org/eclipse/m2e/internal/slf4j/bridge/LogListener.java
@@ -8,131 +8,112 @@
  * Contributors:
  *      Sonatype, Inc. - initial API and implementation
  *******************************************************************************/
+
 package org.eclipse.m2e.internal.slf4j.bridge;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.ILogListener;
 import org.eclipse.core.runtime.IStatus;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-class LogListener
-    implements ILogListener
-{
 
-    public void logging( IStatus status, String plugin )
-    {
-        String name = status.getPlugin();
-        if ( name == null )
-        {
-            name = plugin;
-        }
+class LogListener implements ILogListener {
 
-        Logger logger = LoggerFactory.getLogger( name );
-
-        Log log;
-        switch ( status.getSeverity() )
-        {
-            case IStatus.ERROR:
-                log = ERROR;
-                break;
-            case IStatus.WARNING:
-                log = WARN;
-                break;
-            default:
-                log = INFO;
-                break;
-        }
-
-        if ( !log.isEnabled( logger ) )
-        {
-            return;
-        }
-
-        log( log, logger, status );
+  public void logging(IStatus status, String plugin) {
+    String name = status.getPlugin();
+    if(name == null) {
+      name = plugin;
     }
 
-    private void log( Log log, Logger logger, IStatus status )
-    {
-        if ( status == null )
-        {
-            return;
-        }
+    Logger logger = LoggerFactory.getLogger(name);
 
-        Throwable exception = status.getException();
-
-        log.log( logger, status.getMessage(), exception );
-
-        if ( exception instanceof CoreException )
-        {
-            log( log, logger, ( (CoreException) exception ).getStatus() );
-        }
-
-        if ( status.isMultiStatus() )
-        {
-            IStatus[] children = status.getChildren();
-            if ( children != null )
-            {
-                for ( int i = 0; i < children.length; i++ )
-                {
-                    log( log, logger, children[i] );
-                }
-            }
-        }
+    Log log;
+    switch(status.getSeverity()) {
+      case IStatus.ERROR:
+        log = ERROR;
+        break;
+      case IStatus.WARNING:
+        log = WARN;
+        break;
+      default:
+        log = INFO;
+        break;
     }
 
-    private static interface Log
-    {
-
-        boolean isEnabled( Logger logger );
-
-        void log( Logger logger, String message, Throwable exception );
-
+    if(!log.isEnabled(logger)) {
+      return;
     }
 
-    private static final Log ERROR = new Log()
-    {
+    log(log, logger, status);
+  }
 
-        public boolean isEnabled( Logger logger )
-        {
-            return logger.isErrorEnabled();
+  private void log(Log log, Logger logger, IStatus status) {
+    if(status == null) {
+      return;
+    }
+
+    Throwable exception = status.getException();
+
+    log.log(logger, status.getMessage(), exception);
+
+    if(exception instanceof CoreException) {
+      log(log, logger, ((CoreException) exception).getStatus());
+    }
+
+    if(status.isMultiStatus()) {
+      IStatus[] children = status.getChildren();
+      if(children != null) {
+        for(int i = 0; i < children.length; i++ ) {
+          log(log, logger, children[i]);
         }
+      }
+    }
+  }
 
-        public void log( Logger logger, String message, Throwable exception )
-        {
-            logger.error( message, exception );
-        }
+  private static interface Log {
 
-    };
+    boolean isEnabled(Logger logger);
 
-    private static final Log WARN = new Log()
-    {
+    void log(Logger logger, String message, Throwable exception);
 
-        public boolean isEnabled( Logger logger )
-        {
-            return logger.isWarnEnabled();
-        }
+  }
 
-        public void log( Logger logger, String message, Throwable exception )
-        {
-            logger.warn( message, exception );
-        }
+  private static final Log ERROR = new Log() {
 
-    };
+    public boolean isEnabled(Logger logger) {
+      return logger.isErrorEnabled();
+    }
 
-    private static final Log INFO = new Log()
-    {
+    public void log(Logger logger, String message, Throwable exception) {
+      logger.error(message, exception);
+    }
 
-        public boolean isEnabled( Logger logger )
-        {
-            return logger.isInfoEnabled();
-        }
+  };
 
-        public void log( Logger logger, String message, Throwable exception )
-        {
-            logger.info( message, exception );
-        }
+  private static final Log WARN = new Log() {
 
-    };
+    public boolean isEnabled(Logger logger) {
+      return logger.isWarnEnabled();
+    }
+
+    public void log(Logger logger, String message, Throwable exception) {
+      logger.warn(message, exception);
+    }
+
+  };
+
+  private static final Log INFO = new Log() {
+
+    public boolean isEnabled(Logger logger) {
+      return logger.isInfoEnabled();
+    }
+
+    public void log(Logger logger, String message, Throwable exception) {
+      logger.info(message, exception);
+    }
+
+  };
 
 }
