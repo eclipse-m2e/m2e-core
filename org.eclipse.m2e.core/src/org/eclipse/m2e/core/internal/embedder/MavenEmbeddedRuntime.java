0d81275ea3c2c89ce6a203ab6ddbb3500016cd3e
Igor Fedorenko <igor@ifedorenko.com>
2011-02-10 22:14:22 -0500

    335972 collect embedded maven runtime bundle from multiple bundles
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

--- org.eclipse.m2e.core/src/org/eclipse/m2e/core/internal/embedder/MavenEmbeddedRuntime.java
+++ org.eclipse.m2e.core/src/org/eclipse/m2e/core/internal/embedder/MavenEmbeddedRuntime.java
@@ -14,14 +14,18 @@ package org.eclipse.m2e.core.internal.embedder;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Enumeration;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Properties;
+import java.util.Set;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleException;
 import org.osgi.framework.Constants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,11 +34,18 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.osgi.service.resolver.BaseDescription;
+import org.eclipse.osgi.service.resolver.BundleDescription;
+import org.eclipse.osgi.service.resolver.ExportPackageDescription;
+import org.eclipse.osgi.service.resolver.State;
+import org.eclipse.osgi.service.resolver.VersionConstraint;
+import org.eclipse.osgi.util.ManifestElement;
 
 import org.eclipse.m2e.core.embedder.IMavenLauncherConfiguration;
 import org.eclipse.m2e.core.embedder.MavenRuntime;
 import org.eclipse.m2e.core.embedder.MavenRuntimeManager;
 import org.eclipse.m2e.core.internal.Messages;
+
 
 /**
  * Embedded Maven runtime
@@ -52,37 +63,39 @@ public class MavenEmbeddedRuntime implements MavenRuntime {
   public static final String PLEXUS_CLASSWORLD_NAME = "plexus.core"; //$NON-NLS-1$
 
   private static String[] LAUNCHER_CLASSPATH;
+
   private static String[] CLASSPATH;
 
   private static volatile String mavenVersion;
 
   private BundleContext bundleContext;
-  
+
   public MavenEmbeddedRuntime(BundleContext bundleContext) {
     this.bundleContext = bundleContext;
   }
-  
+
   public boolean isEditable() {
     return false;
   }
-  
+
   public String getLocation() {
     return MavenRuntimeManager.EMBEDDED;
   }
-  
+
   public String getSettings() {
     return null;
   }
-  
+
   public boolean isAvailable() {
     return true;
   }
 
-  public void createLauncherConfiguration(IMavenLauncherConfiguration collector, IProgressMonitor monitor) throws CoreException {
+  public void createLauncherConfiguration(IMavenLauncherConfiguration collector, IProgressMonitor monitor)
+      throws CoreException {
     collector.setMainType(MAVEN_EXECUTOR_CLASS, PLEXUS_CLASSWORLD_NAME);
-    
+
     initClasspath(findMavenEmbedderBundle());
-    
+
     collector.addRealm(IMavenLauncherConfiguration.LAUNCHER_REALM);
     for(String entry : LAUNCHER_CLASSPATH) {
       collector.addArchiveEntry(entry);
@@ -95,35 +108,119 @@ public class MavenEmbeddedRuntime implements MavenRuntime {
         collector.addArchiveEntry(entry);
       }
     }
- }
+  }
 
-  private static synchronized void initClasspath(Bundle bundle) {
+  private synchronized void initClasspath(Bundle mavenRuntimeBundle) {
     if(CLASSPATH == null) {
+      LinkedHashSet<String> allentries = new LinkedHashSet<String>();
+
+      addBundleClasspathEntries(allentries, mavenRuntimeBundle);
+
+      // find and add more bundles
+      State state = Platform.getPlatformAdmin().getState(false);
+      BundleDescription description = state.getBundle(mavenRuntimeBundle.getBundleId());
+      for(String sname : new String[] {"com.ning.async-http-client", "org.jboss.netty", "org.slf4j.api"}) {
+        Bundle dependency = findDependencyBundle(description, sname, new HashSet<BundleDescription>());
+        if(dependency != null) {
+          addBundleClasspathEntries(allentries, dependency);
+        } else {
+          log.warn(
+              "Could not find SOGi bundle with symbolic name ``{}'' required to launch embedded maven runtime in external process",
+              sname);
+        }
+      }
+
       List<String> cp = new ArrayList<String>();
       List<String> lcp = new ArrayList<String>();
 
-      @SuppressWarnings("unchecked")
-      Enumeration<URL> entries = bundle.findEntries("/", "*", true); //$NON-NLS-1$ //$NON-NLS-2$
-      while(entries.hasMoreElements()) {
-        URL url = entries.nextElement();
-        String path = url.getPath();
-        if(path.endsWith(".jar") || path.endsWith("bin/")) { //$NON-NLS-1$ //$NON-NLS-2$
-          try {
-            String file = FileLocator.toFileURL(url).getFile();
-            if (file.contains("plexus-classworlds")) { //$NON-NLS-1$
-              lcp.add(file);
-            } else {
-              cp.add(file);
-            }
-          } catch(IOException ex) {
-            log.error("Error adding classpath entry " + url.toString(), ex);
-          }
+      for(String entry : allentries) {
+        if(entry.contains("plexus-classworlds")) { //$NON-NLS-1$
+          lcp.add(entry);
+        } else {
+          cp.add(entry);
         }
       }
 
       CLASSPATH = cp.toArray(new String[cp.size()]);
       LAUNCHER_CLASSPATH = lcp.toArray(new String[lcp.size()]);
     }
+  }
+
+  private Bundle findDependencyBundle(BundleDescription bundleDescription, String dependencyName,
+      Set<BundleDescription> visited) {
+    ArrayList<VersionConstraint> dependencies = new ArrayList<VersionConstraint>();
+    dependencies.addAll(Arrays.asList(bundleDescription.getRequiredBundles()));
+    dependencies.addAll(Arrays.asList(bundleDescription.getImportPackages()));
+    for(VersionConstraint requiredSpecification : dependencies) {
+      BundleDescription requiredDescription = getDependencyBundleDescription(requiredSpecification);
+      if(requiredDescription != null && visited.add(requiredDescription)) {
+        if(dependencyName.equals(requiredDescription.getName())) {
+          return bundleContext.getBundle(requiredDescription.getBundleId());
+        }
+        Bundle required = findDependencyBundle(requiredDescription, dependencyName, visited);
+        if(required != null) {
+          return required;
+        }
+      }
+    }
+    return null;
+  }
+
+  private BundleDescription getDependencyBundleDescription(VersionConstraint requiredSpecification) {
+    BaseDescription supplier = requiredSpecification.getSupplier();
+    if(supplier instanceof BundleDescription) {
+      return (BundleDescription) supplier;
+    } else if(supplier instanceof ExportPackageDescription) {
+      return ((ExportPackageDescription) supplier).getExporter();
+    }
+    return null;
+  }
+
+  private void addBundleClasspathEntries(Set<String> entries, Bundle bundle) {
+    for(String cp : parseBundleClasspath(bundle)) {
+      String entry;
+      if(".".equals(cp)) {
+        entry = getNestedJarOrDir(bundle, "/");
+      } else {
+        entry = getNestedJarOrDir(bundle, cp);
+      }
+
+      if(entry != null) {
+        entries.add(entry);
+      }
+    }
+  }
+
+  private String[] parseBundleClasspath(Bundle bundle) {
+    String[] result = new String[] {"."};
+    String header = bundle.getHeaders().get(Constants.BUNDLE_CLASSPATH);
+    ManifestElement[] classpathEntries = null;
+    try {
+      classpathEntries = ManifestElement.parseHeader(Constants.BUNDLE_CLASSPATH, header);
+    } catch(BundleException ex) {
+      log.warn("Could not parse bundle classpath of {}", bundle.toString(), ex);
+    }
+    if(classpathEntries != null) {
+      result = new String[classpathEntries.length];
+      for(int i = 0; i < classpathEntries.length; i++ ) {
+        result[i] = classpathEntries[i].getValue();
+      }
+    }
+    return result;
+  }
+
+  private String getNestedJarOrDir(Bundle bundle, String cp) {
+    URL url = bundle.getEntry(cp);
+    if(url == null) {
+      log.debug("Bundle {} does not have entry {}", bundle.toString(), cp);
+      return null;
+    }
+    try {
+      return FileLocator.toFileURL(url).getFile();
+    } catch(IOException ex) {
+      log.warn("Could not get entry {} for bundle {}", new Object[] {cp, bundle.toString(), ex});
+    }
+    return null;
   }
 
   private Bundle findMavenEmbedderBundle() {
@@ -143,16 +240,16 @@ public class MavenEmbeddedRuntime implements MavenRuntime {
 
     StringBuilder sb = new StringBuilder();
     sb.append("Embedded (").append(getVersion()); //$NON-NLS-1$
-    if (embedder != null) {
+    if(embedder != null) {
       String version = (String) embedder.getHeaders().get(Constants.BUNDLE_VERSION);
       sb.append('/').append(version);
     }
     sb.append(')');
 
-    return  sb.toString();
+    return sb.toString();
   }
 
-  private static synchronized String getVersion(Bundle bundle) {
+  private synchronized String getVersion(Bundle bundle) {
     if(mavenVersion != null) {
       return mavenVersion;
     }
@@ -187,7 +284,7 @@ public class MavenEmbeddedRuntime implements MavenRuntime {
         zip.close();
       }
     } catch(Exception e) {
-      log.error("Could not determine embedded maven version", e);
+      log.warn("Could not determine embedded maven version", e);
     }
 
     return Messages.MavenEmbeddedRuntime_unknown;
