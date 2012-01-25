554e77b2d9fa56c3eaffa165f76d9020bdf4b25b
Igor Fedorenko <igor@ifedorenko.com>
2012-01-25 17:11:26 -0500

    369752 fixed stale classpath containers not removed during update
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

--- org.eclipse.m2e.jdt/src/org/eclipse/m2e/jdt/internal/AbstractJavaProjectConfigurator.java
+++ org.eclipse.m2e.jdt/src/org/eclipse/m2e/jdt/internal/AbstractJavaProjectConfigurator.java
@@ -153,18 +153,6 @@ public abstract class AbstractJavaProjectConfigurator extends AbstractProjectCon
 
   protected void addCustomClasspathEntries(IJavaProject javaProject, IClasspathDescriptor classpath)
       throws JavaModelException {
-    //Preserve existing libraries and classpath order (sort of) 
-    // as other containers would have been added AFTER the JRE and M2 ones anyway 
-    IClasspathEntry[] cpEntries = javaProject.getRawClasspath();
-    if(cpEntries != null && cpEntries.length > 0) {
-      for(IClasspathEntry entry : cpEntries) {
-        if(IClasspathEntry.CPE_CONTAINER == entry.getEntryKind()
-            && !JavaRuntime.JRE_CONTAINER.equals(entry.getPath().segment(0))
-            && !MavenClasspathHelpers.isMaven2ClasspathContainer(entry.getPath())) {
-          classpath.addEntry(entry);
-        }
-      }
-    }
   }
 
   protected void invokeJavaProjectConfigurators(IClasspathDescriptor classpath, ProjectConfigurationRequest request,
