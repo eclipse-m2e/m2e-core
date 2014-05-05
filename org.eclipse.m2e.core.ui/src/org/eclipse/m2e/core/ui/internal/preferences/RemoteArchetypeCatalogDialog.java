5b79ef5849fa27889e6aa80b6337f6912a761ed8
Fred Bricon <fbricon@gmail.com>
2014-05-05 16:55:17 +0200

    RemoteArchetypeCatalogDialog : Enable the verify button on Edit
    Change-Id: Ic842cd41fd09425b888b3f2350748616b9db9aa4
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- org.eclipse.m2e.core.ui/src/org/eclipse/m2e/core/ui/internal/preferences/RemoteArchetypeCatalogDialog.java
+++ org.eclipse.m2e.core.ui/src/org/eclipse/m2e/core/ui/internal/preferences/RemoteArchetypeCatalogDialog.java
@@ -100,6 +100,7 @@ public class RemoteArchetypeCatalogDialog extends TitleAreaDialog {
     Control control = super.createContents(parent);
     setTitle(title);
     setMessage(message);
+    update();
     return control;
   }
 
@@ -228,7 +229,6 @@ public class RemoteArchetypeCatalogDialog extends TitleAreaDialog {
   public void create() {
     super.create();
     getButton(IDialogConstants.OK_ID).setEnabled(false);
-    getButton(VERIFY_ID).setEnabled(false);
   }
 
   protected void okPressed() {
@@ -271,14 +271,13 @@ public class RemoteArchetypeCatalogDialog extends TitleAreaDialog {
     setMessage(null, IStatus.WARNING);
 
     String url = catalogUrlCombo.getText().trim();
-    if(url.length() == 0) {
+    boolean isValid = !url.isEmpty();
+    if(!isValid) {
       setErrorMessage(Messages.RemoteArchetypeCatalogDialog_error_required);
-      verifyButton.setEnabled(false);
-      return false;
     }
 
-    verifyButton.setEnabled(true);
-    return true;
+    verifyButton.setEnabled(isValid);
+    return isValid;
   }
 
 }
