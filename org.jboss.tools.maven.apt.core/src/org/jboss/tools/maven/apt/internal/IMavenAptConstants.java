a9954a47affaa9046963cfe1d55f0313b4bd59b6
Fred Bricon <fbricon@gmail.com>
2014-05-20 19:50:31 +0200

    Fix compilation error
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

---
+++ org.jboss.tools.maven.apt.core/src/org/jboss/tools/maven/apt/internal/IMavenAptConstants.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Red Hat, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *      Red Hat, Inc. - initial API and implementation
+ *******************************************************************************/
+package org.jboss.tools.maven.apt.internal;
+
+/**
+ * IMavenAptConstants
+ *
+ * @author Fred Bricon
+ * @since 1.1
+ */
+public interface IMavenAptConstants {
+
+  public static final String PLUGIN_ID = "org.jboss.tools.maven.apt";//$NON-NLS-1$
+  
+  public static final String INVALID_ARGUMENT_MARKER_ID = PLUGIN_ID + ".problem.invalidArgument"; //$NON-NLS-1$
+
+}
