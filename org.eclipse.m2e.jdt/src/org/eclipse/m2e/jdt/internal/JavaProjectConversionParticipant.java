ab8b456c662aba8cecaaa27d5d1dc7a23a86cbb8
Fred Bricon <fbricon@gmail.com>
2018-07-31 15:32:26 -0400

    Update maven plugin versions in templates and conversion
    Change-Id: Iea03acb3cee81c94fff70e6dd455be07bebb72be
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- org.eclipse.m2e.jdt/src/org/eclipse/m2e/jdt/internal/JavaProjectConversionParticipant.java
+++ org.eclipse.m2e.jdt/src/org/eclipse/m2e/jdt/internal/JavaProjectConversionParticipant.java
@@ -82,7 +82,7 @@ public class JavaProjectConversionParticipant extends AbstractProjectConversionP
 
   private static final String COMPILER_ARTIFACT_ID = "maven-compiler-plugin"; //$NON-NLS-1$
 
-  private static final String DEFAULT_COMPILER_VERSION = "3.7.0"; //$NON-NLS-1$
+  private static final String DEFAULT_COMPILER_VERSION = "3.8.0"; //$NON-NLS-1$
 
   private static final String TARGET_KEY = "target"; //$NON-NLS-1$
 
