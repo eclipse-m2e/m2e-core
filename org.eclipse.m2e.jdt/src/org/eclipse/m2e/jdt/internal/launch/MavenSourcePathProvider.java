1f82b4133a0ac992d90689df6914e54f45e37945
Mickael Istria <mistria@redhat.com>
2020-03-31 11:55:19 -0500

    Revert "Bug 559797 - Delegate to class file editor when no source is found"
    This reverts commit 7deeca873433e5d2bcf7756edb60da3194b0df86.
    
    Change-Id: I3535c16cb3364a787e125111560825d4427d34a6
    Signed-Off-By: Mickael Istria <mistria@redhat.com>

--- org.eclipse.m2e.jdt/src/org/eclipse/m2e/jdt/internal/launch/MavenSourcePathProvider.java
+++ org.eclipse.m2e.jdt/src/org/eclipse/m2e/jdt/internal/launch/MavenSourcePathProvider.java
@@ -13,10 +13,20 @@
 
 package org.eclipse.m2e.jdt.internal.launch;
 
+import java.util.Set;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
 import org.eclipse.jdt.launching.IRuntimeClasspathEntry;
+import org.eclipse.jdt.launching.JavaRuntime;
 
 
 public class MavenSourcePathProvider extends MavenRuntimeClasspathProvider {
@@ -30,4 +40,14 @@ public class MavenSourcePathProvider extends MavenRuntimeClasspathProvider {
     // recover persisted source path
     return recoverRuntimePath(configuration, IJavaLaunchConfigurationConstants.ATTR_SOURCE_PATH);
   }
+
+  @Override
+  protected void addProjectEntries(Set<IRuntimeClasspathEntry> resolved, IPath path, int scope, String classifier,
+      ILaunchConfiguration launchConfiguration, final IProgressMonitor monitor, int classpathProperty) {
+    IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+    IProject project = root.getProject(path.segment(0));
+    IJavaProject javaProject = JavaCore.create(project);
+    resolved.add(JavaRuntime.newProjectRuntimeClasspathEntry(javaProject));
+  }
+
 }
