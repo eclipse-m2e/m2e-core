d930bcbdff3d0c707341cbb9d39a4d5710202c3d
Fred Bricon <fbricon@gmail.com>
2020-05-27 11:08:44 +0200

    Project converter to use m-c-p 3.8.1 by default
    Change-Id: I5ba94b26d1af1b26d1627822349183d178c40241
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- org.eclipse.m2e.jdt/src/org/eclipse/m2e/jdt/internal/JavaProjectConversionParticipant.java
+++ org.eclipse.m2e.jdt/src/org/eclipse/m2e/jdt/internal/JavaProjectConversionParticipant.java
@@ -84,7 +84,7 @@ public class JavaProjectConversionParticipant extends AbstractProjectConversionP
 
   private static final String COMPILER_ARTIFACT_ID = "maven-compiler-plugin"; //$NON-NLS-1$
 
-  private static final String DEFAULT_COMPILER_VERSION = "3.8.0"; //$NON-NLS-1$
+  private static final String DEFAULT_COMPILER_VERSION = "3.8.1"; //$NON-NLS-1$
 
   private static final String TARGET_KEY = "target"; //$NON-NLS-1$
 
