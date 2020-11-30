c9d2a4ae64604ba1ecd738612a2ae0023f52eec7
Christoph Läubrich <laeubi@laeubi-soft.de>
2020-11-30 20:01:13 +0100

    'Update' should update to the latest version in repository
    Signed-off-by: Christoph Läubrich <laeubi@laeubi-soft.de>

--- org.eclipse.m2e.pde.ui/src/org/eclipse/m2e/pde/ui/MavenTargetLocationUpdater.java
+++ org.eclipse.m2e.pde.ui/src/org/eclipse/m2e/pde/ui/MavenTargetLocationUpdater.java
@@ -12,6 +12,7 @@
  *******************************************************************************/
 package org.eclipse.m2e.pde.ui;
 
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
@@ -30,8 +31,27 @@ public class MavenTargetLocationUpdater implements ITargetLocationUpdater {
 	@Override
 	public IStatus update(ITargetDefinition target, ITargetLocation targetLocation, IProgressMonitor monitor) {
 		MavenTargetLocation location = (MavenTargetLocation) targetLocation;
-		location.refresh();
-		return Status.OK_STATUS;
+		try {
+			MavenTargetLocation update = location.update(monitor);
+			if (update == null) {
+				return new Status(IStatus.OK, Activator.ID, ITargetLocationUpdater.STATUS_CODE_NO_CHANGE, "", null);
+			}
+			ITargetLocation[] targetLocations = target.getTargetLocations();
+			if (targetLocation == null) {
+				targetLocations = new ITargetLocation[] { update };
+			} else {
+				for (int i = 0; i < targetLocations.length; i++) {
+					if (targetLocations[i] == targetLocation) {
+						targetLocations[i] = update;
+						break;
+					}
+				}
+			}
+			target.setTargetLocations(targetLocations);
+			return Status.OK_STATUS;
+		} catch (CoreException e) {
+			return e.getStatus();
+		}
 	}
 
 }
