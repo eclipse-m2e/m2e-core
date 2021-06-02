b03ad83dac472f146c06edf8e81dc2e6c6c205d3
Mickael Istria <mistria@redhat.com>
2021-06-02 20:00:36 +0200

    Issue #182 - Test empty console

---
+++ org.eclipse.m2e.core.ui.tests/src/org/eclipse/m2e/core/ui/tests/AllTests.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ * - Mickael Istria (Red Hat Inc.) - Initial implementation
+ *******************************************************************************/
+package org.eclipse.m2e.core.ui.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({ConsoleTest.class})
+public class AllTests {
+
+}
