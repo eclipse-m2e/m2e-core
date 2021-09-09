e53c3ff132d54d73d535e6891dedc2839b021fbd
Hannes Wellmann <wellmann.hannes1@gmx.net>
2021-09-09 22:30:54 +0200

    Replace deprecated File.toURL() by File.toURI().toURL()
    File.toURL() does not automatically escape characters that are illegal
    in URLs and it is therefore recommended to use File.toURI().toURL()
    instead.

--- org.eclipse.m2e.logback.configuration/src/org/eclipse/m2e/logback/configuration/LogPlugin.java
+++ org.eclipse.m2e.logback.configuration/src/org/eclipse/m2e/logback/configuration/LogPlugin.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 Sonatype, Inc.
+ * Copyright (c) 2010, 2021 Sonatype, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -130,7 +130,7 @@ public class LogPlugin extends Plugin {
       if(System.getProperty(PROPERTY_LOG_DIRECTORY, "").length() <= 0) { //$NON-NLS-1$
         System.setProperty(PROPERTY_LOG_DIRECTORY, stateDir.getAbsolutePath());
       }
-      loadConfiguration(configFile.toURL());
+      loadConfiguration(configFile.toURI().toURL());
 
       isConfigured = true;
     } catch(Exception e) {
