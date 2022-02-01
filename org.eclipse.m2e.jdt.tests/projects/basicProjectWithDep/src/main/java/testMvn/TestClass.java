20ab753e8b7c3370424dd73b352b3aa23ac9cae4
Alexander Kurtakov <akurtako@redhat.com>
2022-02-01 12:49:39 +0200

    Don't use Log4j 1.2.17
    Update test to log4j2 which doesn't has CVEs.
    
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- org.eclipse.m2e.jdt.tests/projects/basicProjectWithDep/src/main/java/testMvn/TestClass.java
+++ org.eclipse.m2e.jdt.tests/projects/basicProjectWithDep/src/main/java/testMvn/TestClass.java
@@ -2,18 +2,25 @@ package testMvn;
 
 import java.io.StringWriter;
 
-import org.apache.log4j.Appender;
-import org.apache.log4j.PatternLayout;
-import org.apache.log4j.WriterAppender;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.Appender;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.appender.WriterAppender;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.layout.PatternLayout;
 
 public class TestClass {
 
 	public static void main(String[] args) {
 		StringWriter errorOutputWriter = new StringWriter();
-		Appender appender = new WriterAppender(new PatternLayout(PatternLayout.TTCC_CONVERSION_PATTERN),
-				errorOutputWriter);
+		Appender appender = WriterAppender.newBuilder()
+				.setLayout(PatternLayout.newBuilder().withPattern(PatternLayout.TTCC_CONVERSION_PATTERN).build())
+				.setTarget(errorOutputWriter).setName("testMvn").build();
 
-		org.apache.log4j.Logger.getRootLogger().addAppender(appender);
+		final LoggerContext context = LoggerContext.getContext(false);
+	    final Configuration config = context.getConfiguration();
+	    config.addAppender(appender);
+	    config.getRootLogger().addAppender(appender, Level.ALL, null);
 		System.out.print("ok");
 	}
 
