53a40c8044c2e4718338647d63a3408b24ded057
Mickael Istria <mistria@redhat.com>
2022-05-24 11:20:49 +0200

    Remove IMavenExecutionContextFactory
    This interface is never used directly and it doesn't seem profitable to
    introduce a new API and more complex type hierarchy for that. This will
    allow easier changes in the future.

--- org.eclipse.m2e.core/src/org/eclipse/m2e/core/project/IMavenProjectFacade.java
+++ org.eclipse.m2e.core/src/org/eclipse/m2e/core/project/IMavenProjectFacade.java
@@ -30,7 +30,7 @@ import org.apache.maven.project.MavenProject;
 import org.eclipse.m2e.core.embedder.ArtifactKey;
 import org.eclipse.m2e.core.embedder.ArtifactRef;
 import org.eclipse.m2e.core.embedder.ArtifactRepositoryRef;
-import org.eclipse.m2e.core.embedder.IMavenExecutionContextFactory;
+import org.eclipse.m2e.core.embedder.IMavenExecutionContext;
 import org.eclipse.m2e.core.lifecyclemapping.model.IPluginExecutionMetadata;
 import org.eclipse.m2e.core.project.configurator.MojoExecutionKey;
 
@@ -41,7 +41,7 @@ import org.eclipse.m2e.core.project.configurator.MojoExecutionKey;
  * @noimplement This interface is not intended to be implemented by clients.
  * @author Igor Fedorenko
  */
-public interface IMavenProjectFacade extends IMavenExecutionContextFactory {
+public interface IMavenProjectFacade {
 
   /**
    * Returns project relative paths of resource directories
@@ -157,5 +157,11 @@ public interface IMavenProjectFacade extends IMavenExecutionContextFactory {
   String getLifecycleMappingId();
 
   Map<MojoExecutionKey, List<IPluginExecutionMetadata>> getMojoExecutionMapping();
+
+  /**
+   * @return a project-specific Maven execution context.
+   * @since 2.0
+   */
+  IMavenExecutionContext createExecutionContext();
 
 }
