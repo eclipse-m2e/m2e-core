e35211ae7a90d91b19d6b405ca63d6c5aa426ef6
Hannes Wellmann <wellmann.hannes1@gmx.net>
2022-06-24 23:20:18 +0200

    Clean up MavenArchiver sources and project configuration

--- org.eclipse.m2e.mavenarchiver/src/org/eclipse/m2e/mavenarchiver/internal/JarArchiverConfigurator.java
+++ org.eclipse.m2e.mavenarchiver/src/org/eclipse/m2e/mavenarchiver/internal/JarArchiverConfigurator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008-2020 Sonatype, Inc.
+ * Copyright (c) 2008-2022 Sonatype, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -16,28 +16,26 @@ import org.eclipse.m2e.core.project.configurator.MojoExecutionKey;
 
 /**
  * MavenArchiver Configurator for maven-jar-plugin.<br/>
- * This configurator will generate files (MANIFEST.MF, pom/properties files) 
- * under the project's build output directory. 
+ * This configurator will generate files (MANIFEST.MF, pom/properties files)
+ * under the project's build output directory.
  *
  * @author Fred Bricon
  */
 public class JarArchiverConfigurator extends AbstractMavenArchiverConfigurator {
 
-  @Override
-  protected IPath getOutputDir(IMavenProjectFacade facade) {
-    IPath outputLocation = facade.getOutputLocation();
-    return outputLocation;
-  }
+	@Override
+	protected IPath getOutputDir(IMavenProjectFacade facade) {
+		return facade.getOutputLocation();
+	}
 
-  @Override
-  protected String getArchiverFieldName() {
-    return "jarArchiver";
-  }
+	@Override
+	protected String getArchiverFieldName() {
+		return "jarArchiver";
+	}
 
-  @Override
-  protected MojoExecutionKey getExecutionKey() {
-    MojoExecutionKey key = new MojoExecutionKey("org.apache.maven.plugins", "maven-jar-plugin", "", "jar", null, null);
-    return key;
-  }
+	@Override
+	protected MojoExecutionKey getExecutionKey() {
+		return new MojoExecutionKey("org.apache.maven.plugins", "maven-jar-plugin", "", "jar", null, null);
+	}
 
 }
