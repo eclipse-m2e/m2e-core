966c92e66de342413199414c4f5de10595ef40cd
Konrad Windszus <konrad_w@gmx.de>
2022-10-06 09:52:57 +0200

    Apply feedback from PR
    fix configuration

--- org.eclipse.m2e.pde.connector/src/org/eclipse/m2e/pde/connector/TychoPackagingsConfigurator.java
+++ org.eclipse.m2e.pde.connector/src/org/eclipse/m2e/pde/connector/TychoPackagingsConfigurator.java
@@ -25,15 +25,22 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.m2e.core.project.configurator.AbstractProjectConfigurator;
 import org.eclipse.m2e.core.project.configurator.ProjectConfigurationRequest;
+import org.eclipse.pde.ds.internal.annotations.DSAnnotationVersion;
 import org.eclipse.pde.internal.core.natures.PDE;
 import org.eclipse.pde.internal.core.util.CoreUtility;
+import org.osgi.framework.Version;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+@SuppressWarnings("restriction")
 public class TychoPackagingsConfigurator extends AbstractProjectConfigurator {
 
-    private static final String TYCHO_DS_PLUGIN_GROUP_ID = "org.eclipse.tycho";
+    private static final String TYCHO_GROUP_ID = "org.eclipse.tycho";
     private static final String TYCHO_DS_PLUGIN_ARTIFACT_ID = "tycho-ds-plugin";
     private static final String GOAL_DECLARATIVE_SERVICES = "declarative-services";
 
+    private static final Logger log = LoggerFactory.getLogger(TychoPackagingsConfigurator.class);
+   
     @Override
     public void configure(ProjectConfigurationRequest request, IProgressMonitor monitor) throws CoreException {
         MavenProject mavenProject = request.mavenProject();
@@ -48,37 +55,77 @@ public class TychoPackagingsConfigurator extends AbstractProjectConfigurator {
             if (!project.hasNature(PDE.FEATURE_NATURE)) {
                 CoreUtility.addNatureToProject(project, PDE.FEATURE_NATURE, monitor);
             }
-        } else if ("eclipse-repository".equals(packaging)) {
-            // see org.eclipse.pde.internal.ui.wizards.site.NewSiteProjectCreationOperation
-            if (!project.hasNature(PDE.SITE_NATURE)) {
-                CoreUtility.addNatureToProject(project, PDE.SITE_NATURE, monitor);
-            }
         }
     }
 
     void applyDsConfiguration(ProjectConfigurationRequest request, IProgressMonitor monitor) throws CoreException {
         List<MojoExecution> mojoExecutions = getTychoDsPluginMojoExecutions(request, monitor);
-        for (MojoExecution mojoExecution : mojoExecutions) {
-            // apply PDE configuration for DS
-            IEclipsePreferences prefs = new ProjectScope(request.mavenProjectFacade().getProject())
-                    .getNode(org.eclipse.pde.ds.internal.annotations.Activator.PLUGIN_ID);
-            Xpp3Dom dom = mojoExecution.getConfiguration();
-            Xpp3Dom dsEnabled = dom.getChild("tycho.ds.enabled");
-            if (dsEnabled != null && Boolean.valueOf(dsEnabled.getValue())) {
-                prefs.putBoolean(org.eclipse.pde.ds.internal.annotations.Activator.PREF_ENABLED, true);
-            } else {
-                prefs.putBoolean(org.eclipse.pde.ds.internal.annotations.Activator.PREF_ENABLED, false);
+        if (mojoExecutions.isEmpty()) {
+            return;
+        }
+        if (mojoExecutions.size() > 1) {
+            log.error("Found more than one execution for plugin {}:{} and goal {}, only consider configuration of first one", TYCHO_GROUP_ID, TYCHO_DS_PLUGIN_ARTIFACT_ID, GOAL_DECLARATIVE_SERVICES);
+        }
+        // first mojo execution is relevant
+        Xpp3Dom dom = mojoExecutions.get(0).getConfiguration();
+        // apply PDE configuration for DS
+        IEclipsePreferences prefs = new ProjectScope(request.mavenProjectFacade().getProject())
+                .getNode(org.eclipse.pde.ds.internal.annotations.Activator.PLUGIN_ID);
+        Xpp3Dom dsEnabled = dom.getChild("enabled");
+        final boolean isDsEnabled;
+        if (dsEnabled != null && !dsEnabled.getValue().isEmpty()) {
+            isDsEnabled = Boolean.valueOf(dsEnabled.getValue());
+            prefs.putBoolean(org.eclipse.pde.ds.internal.annotations.Activator.PREF_ENABLED, isDsEnabled);
+        } else {
+            isDsEnabled = false;
+        }
+        if (isDsEnabled) {
+            Xpp3Dom dsVersion = dom.getChild("dsVersion");
+            if (containsExplicitConfigurationValue(dsVersion)) {
+                DSAnnotationVersion version = parseVersion(dsVersion.getValue());
+                if (version != null) {
+                    prefs.put(org.eclipse.pde.ds.internal.annotations.Activator.PREF_SPEC_VERSION, version.name());
+                }
             }
-            Xpp3Dom dsVersion = dom.getChild("tycho.ds.version");
-            if (dsVersion == null && !dsVersion.getValue().isEmpty()) {
-                prefs.put(org.eclipse.pde.ds.internal.annotations.Activator.PREF_SPEC_VERSION, dsVersion.getValue());
+            Xpp3Dom path = dom.getChild("path");
+            if (containsExplicitConfigurationValue(path)) {
+                prefs.put(org.eclipse.pde.ds.internal.annotations.Activator.PREF_PATH, path.getValue());
             }
         }
+    }
+    
+    private boolean containsExplicitConfigurationValue(Xpp3Dom config) {
+        if (config == null) {
+            return false;
+        }
+        String value = config.getValue();
+        // check if value is a property name
+        if (value.startsWith("${")) {
+            return false;
+        }
+        return true;
+    }
+
+    DSAnnotationVersion parseVersion(String version) {
+        if (version.startsWith("V")) {
+            version = version.substring(1).replace('_', '.');
+        }
+        Version osgiCompliantVersion = Version.parseVersion(version);
+        if (osgiCompliantVersion.getMajor() == 1 && osgiCompliantVersion.getMinor() == 1) {
+            return DSAnnotationVersion.V1_1;
+        } else if (osgiCompliantVersion.getMajor() == 1 && osgiCompliantVersion.getMinor() == 2) {
+            return DSAnnotationVersion.V1_2;
+        } else if (osgiCompliantVersion.getMajor() == 1 && osgiCompliantVersion.getMinor() == 3) {
+            return DSAnnotationVersion.V1_3;
+        } else {
+            log.error("Unsupported DS spec version {} found, using default instead", version );
+        }
+        return null;
     }
 
     protected List<MojoExecution> getTychoDsPluginMojoExecutions(ProjectConfigurationRequest request, IProgressMonitor monitor)
             throws CoreException {
-        return request.mavenProjectFacade().getMojoExecutions(TYCHO_DS_PLUGIN_GROUP_ID, TYCHO_DS_PLUGIN_ARTIFACT_ID,
+        return request.mavenProjectFacade().getMojoExecutions(TYCHO_GROUP_ID, TYCHO_DS_PLUGIN_ARTIFACT_ID,
                 monitor, GOAL_DECLARATIVE_SERVICES);
     }
 }
