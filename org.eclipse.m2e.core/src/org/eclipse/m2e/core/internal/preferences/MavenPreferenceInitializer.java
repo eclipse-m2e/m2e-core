452b93f4e31e5af6ac88cfee9964aef201acf32e
Mickael Istria <mistria@redhat.com>
2023-06-26 12:23:45 +0200

    Download sources by default
    Fixes https://github.com/eclipse-m2e/m2e-core/issues/117

--- org.eclipse.m2e.core/src/org/eclipse/m2e/core/internal/preferences/MavenPreferenceInitializer.java
+++ org.eclipse.m2e.core/src/org/eclipse/m2e/core/internal/preferences/MavenPreferenceInitializer.java
@@ -36,7 +36,7 @@ public class MavenPreferenceInitializer extends AbstractPreferenceInitializer {
     store.putBoolean(MavenPreferenceConstants.P_OFFLINE, false);
     store.put(MavenPreferenceConstants.P_GLOBAL_UPDATE_POLICY, MavenPreferenceConstants.GLOBAL_UPDATE_POLICY_DEFAULT);
 
-    store.putBoolean(MavenPreferenceConstants.P_DOWNLOAD_SOURCES, false);
+    store.putBoolean(MavenPreferenceConstants.P_DOWNLOAD_SOURCES, true);
     store.putBoolean(MavenPreferenceConstants.P_DOWNLOAD_JAVADOC, false);
 
     store.put(MavenPreferenceConstants.P_OUTPUT_FOLDER, "target-eclipse"); //$NON-NLS-1$
