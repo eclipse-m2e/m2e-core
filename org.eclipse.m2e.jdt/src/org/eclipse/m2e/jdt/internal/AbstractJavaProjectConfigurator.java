9955a2277bf2c9c8bf3bc8c3e042c54453fb6078
Hannes Wellmann <wellmann.hannes1@gmx.net>
2024-06-03 00:49:44 +0200

    Ignore container encoding when checking if an encoding is already set
    If the container encoding is not ignored it can happen that for example
    a project's encoding is not set if the workspace preference matches the
    encoding defined in the pom.xml.
    
    Follow-up on https://github.com/eclipse-m2e/m2e-core/pull/1758

--- org.eclipse.m2e.jdt/src/org/eclipse/m2e/jdt/internal/AbstractJavaProjectConfigurator.java
+++ org.eclipse.m2e.jdt/src/org/eclipse/m2e/jdt/internal/AbstractJavaProjectConfigurator.java
@@ -478,7 +478,7 @@ public abstract class AbstractJavaProjectConfigurator extends AbstractProjectCon
       }
 
       // Set folder encoding (null = platform/container default)
-      if(sourceFolder.exists() && !Objects.equals(sourceFolder.getDefaultCharset(), sourceEncoding)) {
+      if(sourceFolder.exists() && !Objects.equals(sourceFolder.getDefaultCharset(false), sourceEncoding)) {
         sourceFolder.setDefaultCharset(sourceEncoding, monitor);
       }
 
@@ -573,7 +573,7 @@ public abstract class AbstractJavaProjectConfigurator extends AbstractProjectCon
           addResourceFolder(classpath, path, outputPath, addTestFlag);
         }
         // Set folder encoding (null = platform default)
-        if(r.exists() && !Objects.equals(r.getDefaultCharset(), resourceEncoding)) {
+        if(r.exists() && !Objects.equals(r.getDefaultCharset(false), resourceEncoding)) {
           r.setDefaultCharset(resourceEncoding, monitor);
         }
       } else {
