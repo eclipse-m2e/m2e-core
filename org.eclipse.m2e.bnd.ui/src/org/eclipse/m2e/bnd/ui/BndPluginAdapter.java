4ac921646d3a547aa56558b30c57652e9074a41c
Christoph Läubrich <laeubi@laeubi-soft.de>
2024-07-11 09:02:04 +0200

    Add mavenproject artifacts as implicit repository
    Signed-off-by: Christoph Läubrich <laeubi@laeubi-soft.de>

--- org.eclipse.m2e.bnd.ui/src/org/eclipse/m2e/bnd/ui/BndPluginAdapter.java
+++ org.eclipse.m2e.bnd.ui/src/org/eclipse/m2e/bnd/ui/BndPluginAdapter.java
@@ -12,10 +12,14 @@
  *******************************************************************************/
 package org.eclipse.m2e.bnd.ui;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import org.apache.maven.artifact.Artifact;
 import org.apache.maven.project.MavenProject;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.AdapterTypes;
@@ -29,6 +33,7 @@ import org.osgi.service.component.annotations.Component;
 import aQute.bnd.build.Project;
 import aQute.bnd.build.Workspace;
 import aQute.bnd.maven.lib.configuration.BeanProperties;
+import aQute.bnd.maven.lib.resolve.ImplicitFileSetRepository;
 import aQute.bnd.osgi.Processor;
 
 /**
@@ -60,12 +65,21 @@ public class BndPluginAdapter implements IAdapterFactory {
 							}
 						}
 						Processor processor = new Processor(processorProperties, false);
-
+						Collection<File> list = new ArrayList<File>();
+						if (mp != null) {
+							for (Artifact artifact : mp.getArtifacts()) {
+								list.add(artifact.getFile());
+							}
+						}
+						ImplicitFileSetRepository repository = new ImplicitFileSetRepository("Project Artifacts", list);
 						// TODO propertiesFile = new BndConfiguration(project,
 						// mojoExecution).loadProperties(builder);
+						// TODO
+						// aQute.bnd.maven.lib.resolve.BndrunContainer.getFileSetRepository(MavenProject)
 
 						Workspace standaloneWorkspace = Workspace.createStandaloneWorkspace(processor,
 								mavenProject.getPomFile().getParentFile().toURI());
+						standaloneWorkspace.addBasicPlugin(repository);
 						if (mp == null) {
 							standaloneWorkspace.set("workspaceName", mavenProject.getArtifactKey().toPortableString());
 						} else {
@@ -76,6 +90,7 @@ public class BndPluginAdapter implements IAdapterFactory {
 								standaloneWorkspace.set("workspaceDescription", description);
 							}
 						}
+						standaloneWorkspace.refresh();
 						Project bndProject = new Project(standaloneWorkspace, null, null);
 						bndProject.setBase(null);
 						return adapterType.cast(bndProject);
