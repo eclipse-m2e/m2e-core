0e823b3a0030ca09ac52ca3b0d7f1472b6ecba6d
treilhes <p.treilhes@free.fr>
2024-09-09 16:08:33 +0200

    unit test configuration handles deferred var @{x} from surefire/failsafe

--- org.eclipse.m2e.jdt/src/org/eclipse/m2e/jdt/internal/UnitTestSupport.java
+++ org.eclipse.m2e.jdt/src/org/eclipse/m2e/jdt/internal/UnitTestSupport.java
@@ -24,8 +24,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Properties;
 import java.util.Set;
 import java.util.StringJoiner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -142,6 +145,11 @@ public class UnitTestSupport {
    * maven artifact id for the failsafe plugin
    */
   private static final String FAILSAFE_PLUGIN_ARTIFACT_ID = "maven-failsafe-plugin";
+
+  /**
+   * deffered variable pattern
+   */
+  private static final Pattern DEFERRED_VAR_PATTERN = Pattern.compile("@\\{(.*?)\\}");
 
   /**
    * maven group id for the maven plugins
@@ -398,8 +406,11 @@ public class UnitTestSupport {
         IProgressMonitor monitor) {
       try {
         IMaven maven = MavenPlugin.getMaven();
-        return new TestLaunchArguments(
-            maven.getMojoParameterValue(mavenProject, execution, PLUGIN_ARGLINE, String.class, monitor),
+
+        String argLine = maven.getMojoParameterValue(mavenProject, execution, PLUGIN_ARGLINE, String.class, monitor);
+        argLine = resolveDeferredVariables(mavenProject, argLine);
+
+        return new TestLaunchArguments(argLine,
             maven.getMojoParameterValue(mavenProject, execution, PLUGIN_SYSPROP_VARIABLES, Map.class, monitor),
             maven.getMojoParameterValue(mavenProject, execution, PLUGIN_ENVIRONMENT_VARIABLES, Map.class, monitor),
             maven.getMojoParameterValue(mavenProject, execution, PLUGIN_WORKING_DIRECTORY, File.class, monitor),
@@ -410,6 +421,33 @@ public class UnitTestSupport {
       return null;
     }
 
+  }
+
+  /**
+   * This method is used to resolve deferred variables introduced by failsafe/surefire plugins in a given string value.
+   * Deferred variables are placeholders in the string that are replaced with actual values from the Maven project's
+   * properties. The placeholders are in the format @{...}, where ... is the key of the property. If a placeholder's
+   * corresponding property does not exist, the placeholder is left as is.
+   *
+   * @param mavenProject the Maven project from which to retrieve the properties
+   * @param value the string containing the placeholders to be replaced
+   * @return the string with all resolvable placeholders replaced with their corresponding property values
+   */
+  private static String resolveDeferredVariables(MavenProject mavenProject, String value) {
+    Properties properties = mavenProject.getProperties();
+    Matcher matcher = DEFERRED_VAR_PATTERN.matcher(value);
+
+    while(matcher.find()) {
+      String match = matcher.group();
+      String key = matcher.group(1);
+      String replacement = properties.getProperty(key);
+
+      if(replacement != null) {
+        value = value.replace(match, replacement);
+      }
+    }
+
+    return value;
   }
 
   /**
