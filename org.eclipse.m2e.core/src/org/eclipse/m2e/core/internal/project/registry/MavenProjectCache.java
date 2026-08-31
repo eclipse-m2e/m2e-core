9b7bee19214941a07222e88b7c42bec983f48f91
Morgan Chang <shin19991207@gmail.com>
2026-08-31 15:49:51 -0400

    remove unused import
    Signed-off-by: Morgan Chang <shin19991207@gmail.com>

--- org.eclipse.m2e.core/src/org/eclipse/m2e/core/internal/project/registry/MavenProjectCache.java
+++ org.eclipse.m2e.core/src/org/eclipse/m2e/core/internal/project/registry/MavenProjectCache.java
@@ -25,7 +25,6 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.LongAdder;
 import java.util.function.Function;
 
 import org.osgi.service.component.annotations.Component;
